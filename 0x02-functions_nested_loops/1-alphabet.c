3a58c76e02939c1844e29a56db3d97164442821f
Ron1993 <rabiujoel@gmail.com>
2021-06-23 05:06:20 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main- print lowercase a to z
+ * followed by a new line
+ */
+void print_alphabet(void);
+{
+char nuru = 'a';
+while (nuru <= 'z')
+{
+_putchar(nuru);
+nuru++;
+}
+_putchar('\n');
+}
