15a00290323bfad660b2719ccc4f6548a0151bc2
Ron1993 <rabiujoel@gmail.com>
2021-06-23 04:31:55 -0700

    Holberton please work

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,11 @@
+#include <unistd.h>
+/**
+ * main - write the character c to the standout
+ * @c - the character to print
+ * Return: On success 1 
+ */
+
 int _putchar(char c);
+{
+return (write(1, &c, 1));
+}
